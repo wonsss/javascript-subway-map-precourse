08a80a5b2a6241c3dda6fc75d41294efc6e1f88a
wonsss <bigdatajang@gmail.com>
2021-12-15 16:49:50 +0900

    docs: Write a list of features to implement

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,12 @@
+import * as $ from './util/dom.js';
+
 export default class SubwayController {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+  }
+
+  app() {
+    // this.view.renderInApp('afterbegin', $.);
   }
 }
--- src/view.js
+++ src/view.js
@@ -1,5 +1,10 @@
 export default class SubwayView {
   constructor(model) {
     this.model = model;
+    this.$app = document.getElementById('app');
+  }
+
+  renderInApp(position, html) {
+    this.$app.insertAdjacentHTML(position, html);
   }
 }
